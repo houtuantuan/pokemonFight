50c144903e19638103ae03a6e5442d30192625bb
houtuantuan <80598603+houtuantuan@users.noreply.github.com>
2023-04-12 20:36:14 +0200

    pagination finished

--- pokemonfrontend/src/App.js
+++ pokemonfrontend/src/App.js
@@ -7,30 +7,14 @@ import SinglePokemon from './components/SinglePokemon'
 import DetailedPokemon from "./components/DetailedPokemon"
 
 function App () {
-  const [pokemon, setPokemon] = useState([])
-
-  const fetchData = async () => {
-    try {
-      const getData = await fetch('http://localhost:4000/pokemon')
-      if (!getData)
-        throw new Error(`Request failes with a status of ${getData.status}`)
-      const parseData = await getData.json()
-      setPokemon(parseData)
-    } catch (error) {
-      console.log(error.message)
-    }
-  }
-
-  useEffect(() => {
-    fetchData()
-  }, [])
+ 
   return (
     <>
-      <div className='App'>pokemon</div>
+      <div className='App'></div>
       <Routes>
-        <Route path='/' element={<Pokemon pokemon={pokemon} />}></Route>
-        <Route path="/pokemon/:index" element={<SinglePokemon pokemon={pokemon} />}></Route>
-        <Route path="/pokemon/:index/:info" element={<DetailedPokemon pokemon={pokemon} />}></Route>
+        <Route path='/' element={<Pokemon/>}></Route>
+        <Route path="/pokemon/:index" element={<SinglePokemon/>}></Route>
+        <Route path="/pokemon/:index/:info" element={<DetailedPokemon/>}></Route>
       </Routes>
     </>
   )
--- pokemonfrontend/src/components/Pokemon.js
+++ pokemonfrontend/src/components/Pokemon.js
@@ -1,24 +1,83 @@
 import React from 'react'
 import { Link, useParams } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import ReactPaginate from 'react-paginate'
+import "./pagination.css"
 
-export default function Pokemon ({ pokemon }) {
+export default function Pokemon () {
+  const [pokemon, setPokemon] = useState()
+  // Here we use item offsets; we could also use page offsets
+  // following the API or data you're working with.
+  const [itemOffset, setItemOffset] = useState(0)
+  const itemsPerPage = 21
+  const fetchData = async offset => {
+    try {
+      const getData = await fetch(
+        `http://localhost:4000/pokemon?itemOffset=${offset}&endOffset=${
+          offset + itemsPerPage
+        }`
+      )
+      if (!getData)
+        throw new Error(`Request failes with a status of ${getData.status}`)
+      const parseData = await getData.json()
+      setPokemon(parseData)
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+
+  useEffect(() => {
+    fetchData(itemOffset)
+  }, [])
+
+  
+
+  // Simulate fetching items from another resources.
+  // (This could be items from props; or items loaded in a local state
+  // from an API endpoint with useEffect and useState)
+  const endOffset = itemOffset + itemsPerPage
+  console.log(`Loading items from ${itemOffset} to ${endOffset}`)
+  const pageCount = pokemon ? Math.ceil(pokemon.total / itemsPerPage) : 0
+
+  // Invoke when user click to request another page.
+  const handlePageClick = event => {
+    const newOffset = (event.selected * itemsPerPage) % pokemon.total
+    console.log(
+      `User requested page number ${event.selected}, which is offset ${newOffset}`
+    )
+    fetchData(newOffset)
+    setItemOffset(newOffset)
+  }
+
   return (
-    <div className="maxScreen">
-      <div className="maxDesign">
-        <div className="pmList">
-        {pokemon &&
-
-          pokemon.map((el) => (
-            <Link to={`pokemon/${el.id}`} key={el.id}>
-              <div className="pmCard">
-                <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/${el.id}.png`} /> 
-                <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${el.id}.png`} />
-                {<h4>{el.name.english}</h4>}
-              </div>
-
-            </Link>
-          ))}
+    <div className='maxScreen'>
+      <div className='maxDesign'>
+        <div className='pmList'>
+          {pokemon &&
+            pokemon.current.map(el => (
+              <Link to={`pokemon/${el.id}`} key={el.id}>
+                <div className='pmCard'>
+                  <img
+                    src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/${el.id}.png`}
+                  />
+                  <img
+                    src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${el.id}.png`}
+                  />
+                  {<h4>{el.name}</h4>}
+                </div>
+              </Link>
+            ))}
         </div>
+        <ReactPaginate
+        className="react-paginate"
+          breakLabel='...'
+          nextLabel='next >'
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={5}
+          pageCount={pageCount}
+          previousLabel='< previous'
+          renderOnZeroPageCount={null}
+        />
       </div>
     </div>
   )
