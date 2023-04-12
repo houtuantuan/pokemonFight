9bbe88e7dc077fd71f590167e884a954df46b521
Uwe Wieben <u.wieben@cloud-user-skills.de>
2023-04-12 17:53:13 +0200

    Design übernommen

--- pokemonfrontend/src/components/Pokemon.js
+++ pokemonfrontend/src/components/Pokemon.js
@@ -3,16 +3,21 @@ import { Link, useParams } from 'react-router-dom'
 
 export default function Pokemon ({ pokemon }) {
   return (
-    <>
-      <h2>the names of pokemons</h2>
-      <div>
+    <div className="maxScreen">
+      <div className="maxDesign">
+        <div className="pmList">
         {pokemon &&
-          pokemon.map((el, index) => (
-            <Link to={`pokemon/${index}`} key={index}>
-              {<li>{el.name.english}</li>}
+          pokemon.map((el) => (
+            <Link to={`pokemon/${el.id}`} key={el.id}>
+              <div className="pmCard">
+                <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/${el.id}.png`} /> 
+                <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${el.id}.png`} />
+                {<h4>{el.name.english}</h4>}
+              </div>
             </Link>
           ))}
+        </div>
       </div>
-    </>
+    </div>
   )
 }
